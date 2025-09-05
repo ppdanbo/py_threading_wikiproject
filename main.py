fa062bc3ef0555f780fc1a37ae28bae00f4711c6
skydbw <danbo.wang@gmail.com>
2025-09-04 21:32:37 -0400

    pipeline with loggings

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
+import os
 import time
 import logging
+from dotenv import load_dotenv
 
 from yaml_reader import YamlPipelineExecutor
 from utils.setup_logging import setup_logging
+
 
 ### Add-on with Queue for the threads so we can manage its speed and scale up and down
 ### https://realpython.com/python-concurrency/#threading-and-multiprocessing
@@ -10,14 +13,19 @@ from utils.setup_logging import setup_logging
 def main():
 
     setup_logging()
-    logger = logging.getLogger(__name__)
-    pipeline_location = 'pipelines/wiki_yahoo_pipeline.yaml'   
+    logger = logging.getLogger(__name__)    
+    load_dotenv()    
+    pipeline_location = os.environ.get('PIPELINE_LOCATION')
+    if pipeline_location is None: 
+        logger.error("Pipeline location not defined")
+        exit(1)
     start_time = time.time() 
         
     try: 
         logger.info("Starting pipeline %s", pipeline_location)
         pipeline_executor = YamlPipelineExecutor(pipeline_location)
         pipeline_executor.start()    
+        pipeline_executor.join()
         elapsed  = round(time.time() - start_time, 1)
         logger.info("Total time taken for running the pipeline: %s seconds", elapsed)     
     except Exception as e:
